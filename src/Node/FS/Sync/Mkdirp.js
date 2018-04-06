34fbc8f8e38efeb825edeab1136f24a897b8b025
Ilya Ostrovskiy <iostat@users.noreply.github.com>
2018-04-06 17:13:20 -0400

    attribution to vendored code:

--- src/Node/FS/Sync/Mkdirp.js
+++ src/Node/FS/Sync/Mkdirp.js
@@ -1,5 +1,8 @@
 "use strict";
 
+// too lazy to fork https://github.com/joshuahhh/purescript-mkdirp and PR to update it
+// so I just vendored it instead LOL
+
 exports.mkdirp =
   function (dir) {
     return function () {
--- src/Node/FS/Sync/Mkdirp.purs
+++ src/Node/FS/Sync/Mkdirp.purs
@@ -1,3 +1,5 @@
+-- | too lazy to fork https://github.com/joshuahhh/purescript-mkdirp and PR to update it
+-- | so I just vendored it instead LOL
 module Node.FS.Sync.Mkdirp (mkdirp) where
 
 import Prelude
