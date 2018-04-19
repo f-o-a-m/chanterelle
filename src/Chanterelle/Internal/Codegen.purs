864b025aaa484377b8f217a57905155452132357
Irakli Safareli <i.safareli@gmail.com>
2018-04-19 19:26:37 +0200

    do not swallow errors during abi parsing

--- src/Chanterelle/Internal/Codegen.purs
+++ src/Chanterelle/Internal/Codegen.purs
@@ -3,32 +3,32 @@ module Chanterelle.Internal.Codegen
   ) where
 
 import Prelude
+
+import Chanterelle.Internal.Logging (LogLevel(..), log)
 import Chanterelle.Internal.Types.Compile (CompileError(..))
 import Chanterelle.Internal.Types.Project (ChanterelleProject(..), ChanterelleProjectSpec(..), ChanterelleModule(..))
-import Chanterelle.Internal.Logging (LogLevel(..), log)
 import Chanterelle.Internal.Utils (assertDirectory)
+import Control.Error.Util (note)
+import Control.Monad.Aff.Class (class MonadAff, liftAff)
+import Control.Monad.Aff.Console (CONSOLE)
+import Control.Monad.Error.Class (class MonadThrow, throwError)
+import Data.AbiParser (Abi(Abi), AbiDecodeError(..), AbiWithErrors) as PSWeb3Gen
 import Data.Argonaut (decodeJson)
 import Data.Argonaut.Parser (jsonParser)
 import Data.Argonaut.Prisms (_Object)
-import Control.Error.Util (note)
-import Control.Monad.Aff.Console (CONSOLE)
-import Control.Monad.Aff.Class (class MonadAff, liftAff)
-import Control.Monad.Error.Class (class MonadThrow, throwError)
-import Control.Monad.Writer (runWriter)
-import Data.Either (either)
+import Data.Array (mapMaybe)
+import Data.CodeGen (GeneratorOptions, runImported) as PSWeb3Gen
+import Data.Either (Either(..), either)
+import Data.Generator (genCode) as PSWeb3Gen
+import Data.Identity (Identity(..))
 import Data.Lens ((^?))
 import Data.Lens.Index (ix)
+import Data.Maybe (Maybe(..))
 import Data.Traversable (for)
-import Data.Tuple (Tuple(..))
+import Node.Encoding (Encoding(UTF8))
 import Node.FS.Aff as FS
 import Node.Path (FilePath)
 import Node.Path as Path
-import Node.Encoding (Encoding(UTF8))
-
--- ps-web3-gen imports
-import Data.AbiParser (Abi) as PSWeb3Gen
-import Data.Generator (genCode) as PSWeb3Gen
-import Data.CodeGen (GeneratorOptions, genPSModuleStatement, runImports) as PSWeb3Gen
 
 generatePS :: forall eff m
             . MonadAff (console :: CONSOLE, fs :: FS.FS | eff) m
@@ -38,9 +38,14 @@ generatePS :: forall eff m
 generatePS p@(ChanterelleProject project) = do
   let psArgs = projectPSArgs p
   void <<< for project.modules $ \(ChanterelleModule mod) -> do
-      abi <- loadAbi p mod.jsonPath
-      let psModule = generatePSModule p abi mod.moduleName
+      (PSWeb3Gen.Abi abiWithErrors) <- loadAbi p mod.jsonPath
       log Debug $ "generating purescript for " <> mod.moduleName
+      abi <- for abiWithErrors case _ of
+        Left (PSWeb3Gen.AbiDecodeError err) -> do
+          log Error $ "while parsing abi type of object at index: " <> show err.idx <> " from: " <> mod.jsonPath <> " got error: " <> err.error
+          pure Nothing
+        Right x -> pure $ Just x
+      let psModule = generatePSModule p (PSWeb3Gen.Abi $ mapMaybe (map Identity) abi) mod.moduleName
       assertDirectory (Path.dirname mod.pursPath)
       log Info $ "writing PureScript bindings for " <> mod.moduleName
       liftAff $ FS.writeTextFile UTF8 mod.pursPath psModule
@@ -59,23 +64,20 @@ projectPSArgs (ChanterelleProject project) =
 
 generatePSModule
   :: ChanterelleProject
-  -> PSWeb3Gen.Abi
+  -> PSWeb3Gen.Abi Identity
   -> String
   -> String
 generatePSModule p@(ChanterelleProject project) abi moduleName =
   let (ChanterelleProjectSpec spec) = project.spec
-      psArgs = projectPSArgs p
-      (Tuple code accImports) = runWriter $ PSWeb3Gen.genCode abi {exprPrefix: spec.psGen.exprPrefix, indentationLevel: 0}
-      modStatement = PSWeb3Gen.genPSModuleStatement psArgs moduleName
-      imports = if code == "" then "" else PSWeb3Gen.runImports accImports <> "\n"
-   in modStatement <> "\n" <> imports <> code
+  in PSWeb3Gen.genCode abi {exprPrefix: spec.psGen.exprPrefix, indentationLevel: 0}
+    # PSWeb3Gen.runImported (projectPSArgs p) moduleName
 
 loadAbi :: forall eff m
          . MonadAff (fs :: FS.FS | eff) m
         => MonadThrow CompileError m
         => ChanterelleProject
         -> FilePath
-        -> m PSWeb3Gen.Abi
+        -> m PSWeb3Gen.AbiWithErrors
 loadAbi (ChanterelleProject project) abiFile = do
     let (ChanterelleProjectSpec spec) = project.spec
     ejson <- liftAff (jsonParser <$> FS.readTextFile UTF8 abiFile)
