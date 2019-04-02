7e9360d1339285a2ecb029591c703a727458ecb2
Irakli Safareli <i.safareli@gmail.com>
2019-04-02 09:50:28 +0400

    exit 1

--- chanterelle-bin.sh
+++ chanterelle-bin.sh
@@ -4,14 +4,14 @@
 if [ ! -d './output' ] 
 then 
     echo "No './output' directory, make sure to compile purescript project."
-    exit
+    exit 1
 fi
 
 
 if [ ! -f './output/ChanterelleMain/index.js' ] 
 then 
     echo "Make sure you have purescript-chantrelle in your purescript dependencies and it is compiled."
-    exit
+    exit 1
 fi
 
 node -e "console.log(process.argv);require('./output/ChanterelleMain/index.js').main();" -- "chanterelle" $*
