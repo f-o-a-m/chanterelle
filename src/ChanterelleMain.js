ce1681fda065bd2fe08b537bf856e5456ac5147d
Carl Factora <IvantheTricourne@users.noreply.github.com>
2019-08-22 18:23:44 -0400

    Add error message for improper deploy script naming (#105)
    Closes #104.
    
    The main `deploy` script should be named `deploy`.

--- src/ChanterelleMain.js
+++ src/ChanterelleMain.js
@@ -6,7 +6,7 @@ exports.loadDeployMFromScriptPath = function (filePath) {
     var scriptPath = path.isAbsolute(filePath) ? filePath : path.join (process.cwd(), filePath);
     var script = require(scriptPath).deploy
     if (script == undefined) {
-      throw "Deploy script is invalid: " + scriptPath
+      throw "Deploy script is invalid or module does not export a \"deploy\" function: " + scriptPath
     }
     return script;
   };
